e999c2fa760286b14226321640bd149876a23f04
snoh666 <snochopl@gmail.com>
2019-12-10 09:40:14 +0100

    Progression

--- index.py
+++ index.py
@@ -24,7 +24,30 @@ def firstEx():
   return '4.1: '+ str(howMany)
 
 def secondEx():
-  return '4.2'
+  data = getFile()
+  correctNumbers = []
+
+  for value in data:
+    if len(value) > 0:
+      wholeSum = 0
+      strongSum = 0
+      value = value.strip()
+      for singleNumber in value:
+        for i in range(1, int(singleNumber)): # Factorial with singleNumber
+          if strongSum == 0: # If it's first number (strongSum == 0)
+            strongSum = i
+          else:
+            strongSum *= i
+          if(value == '145'):
+            print(singleNumber, i)
+        # Sum factorial with whole sum and reset factorial counter for next loop
+        wholeSum += strongSum
+        strongSum = 0
+
+      if wholeSum == int(value):
+        correctNumbers.append(int(value))
+
+  return '4.2 ' + str(correctNumbers)
 
 def thirdEx():
   return '4.3'
