abcfb766f5d239a57e2d673ddb98ae078068a13b
snoh666 <snochopl@gmail.com>
2019-12-10 09:25:36 +0100

    Imported math and end of firstEx()

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+import math
+
 def getFile():
   with open('przyklad.txt') as file:
     data = file.read()
@@ -7,8 +9,19 @@ def getFile():
 
 
 def firstEx():
-  print(getFile())
-  return '4.1'
+  data = getFile()
+  howMany = 0
+
+  for value in data: # Loop throgh values
+    if len(value) > 0:
+      sqrNumber = int(value) # Set value to int
+      if sqrNumber >= 0: # Do it if its higher than 3
+        while sqrNumber > 4:
+          sqrNumber = math.sqrt(sqrNumber)
+        if sqrNumber == 3:
+          howMany += 1
+
+  return '4.1: '+ str(howMany)
 
 def secondEx():
   return '4.2'
